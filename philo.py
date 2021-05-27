6e5f6d46cf8ad4366dad4b590c1d9425aa06da4a
cyril charignon <cyril.charignon@net-c.com>
2021-05-27 10:16:40 +0200

    initial

---
+++ philo.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+import requests
+import os
+import json
+import csv
+import pprint
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+from sklearn.manifold import TSNE
+# scrapping
+import bs4
+#NLP
+import string
+from nltk import word_tokenize
+import fasttext
